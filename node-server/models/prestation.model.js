2abfb354212c8ebf0966cc6335f05f261817f050
Loick Fricker <loickfricker@gmail.com>
2024-02-16 09:26:07 +0100

    fix

--- node-server/models/prestation.model.js
+++ node-server/models/prestation.model.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const prestationSchema = new mongoose.Schema({
-    title: {String, required: true },
-    description: {String, required: true },
-    price: {Number, required: true },
-    duration: {Number, required: true },
-    image: {Buffer, required: true }
+    title: String,
+    description: String,
+    price: Number,
+    duration: Number,
+    image: Buffer
 });
 
 module.exports = mongoose.model('Prestation', prestationSchema);
